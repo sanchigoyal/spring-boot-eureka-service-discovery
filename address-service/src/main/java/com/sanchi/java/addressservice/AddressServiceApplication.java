11262c7b8a05c774d042444ce8b9a400eedec865
sanchigoyal <sanchi_goyal@yahoo.in>
2019-03-30 18:40:54 -0700

    First commit

---
+++ address-service/src/main/java/com/sanchi/java/addressservice/AddressServiceApplication.java
@@ -0,0 +1,15 @@
+package com.sanchi.java.addressservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class AddressServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressServiceApplication.class, args);
+	}
+
+}
---
+++ mail-service/src/main/java/com/sanchi/java/mailservice/MailServiceApplication.java
@@ -0,0 +1,24 @@
+package com.sanchi.java.mailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class MailServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MailServiceApplication.class, args);
+  }
+
+  @LoadBalanced
+  @Bean
+  public RestTemplate getRestTemplate() {
+    return new RestTemplate();
+  }
+
+}
